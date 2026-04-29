e7747590ecfd6767b36f3154c51878a7fd1f9f0c
Tom <tomdanwills05@gmail.com>
2026-04-29 19:54:02 +0100

    Improved output format and added traditional flags for input.
    Imported package Figure for ASCII banner. Added -hostname and -time flags.

--- main.go
+++ main.go
@@ -3,18 +3,45 @@ package main
 import (
 	"fmt"
 	"time"
+	"flag"
+	"strings"
+	"github.com/common-nighthawk/go-figure"
 
 	"portScanner/scanner"
 )
 
 func main() {
-	
-	timeout := 1*time.Second
+
+	/*
+		Defining our flags
+
+		hostname = takes a hostname value of type string
+
+		timeout = takes a timeout value of type integer
+	*/
+
+	hostname := flag.String("hostname", "", "a string" )
+	timeout := flag.Duration("time", 100*time.Millisecond, "Defines a timeout")
+	flag.Parse()
+
+	executeTime := time.Now().Format(time.RFC850)
+
+	fmt.Println(strings.Repeat("=", 75))
+
+	myFigure := figure.NewFigure("goScanner", "", true)
+	myFigure.Print()
+
+	fmt.Println(strings.Repeat("=", 75))
+	fmt.Printf("Starting goScan v1.0 on %s\n", executeTime)
+	fmt.Printf("Checking ports on %s\n", *hostname)
 
 	for port := 1; port <= 1024; port++ {
+	
+	// If condition calls on CheckPort function within the scanner package
+	// CheckPort requires 3 parameters: hostname, port and timeout
 
-	if scanner.CheckPort("scanme.nmap.org", port, timeout) {
-		fmt.Printf("Port %d is open\n", port)
+	if scanner.CheckPort(*hostname, port, *timeout) {
+		fmt.Printf("%d/tcp		open\n", port)
 		}
 	}
 }
